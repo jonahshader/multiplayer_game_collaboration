ddffff7e57ccb672ee2149cb2ae8e82349581b36
Jonah Shader <jonah.shader@gmail.com>
2019-07-13 18:06:44 -0400

    Initial commit

---
+++ desktop/src/com/compilation/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.compilation.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.compilation.game.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
