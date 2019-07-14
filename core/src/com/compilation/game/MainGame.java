f42ac2b2ded74d3e84d560ce350e5ec97a3b779e
Jonah Shader <jonah.shader@gmail.com>
2019-07-13 20:45:48 -0400

    refactored example code to use Game and Screen

--- core/src/com/compilation/game/MainGame.java
+++ core/src/com/compilation/game/MainGame.java
@@ -1,33 +1,27 @@
 package com.compilation.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.compilation.game.screens.MainMenuScreen;
 
-public class MainGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
+public class MainGame extends Game {
+	public static SpriteBatch batch;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		batch = new SpriteBatch();		// initialize sprite batch
+		setScreen(new MainMenuScreen());		// change screen to main menu
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		super.dispose();
 	}
 }
