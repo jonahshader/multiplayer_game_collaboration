dec256dc7ba9e7d3e3e2ed7013b3c88153c8175a
Jonah Shader <jonah.shader@gmail.com>
2019-07-17 23:10:29 -0400

    Started implementing world chunk. wip

--- core/src/com/compilation/game/world/WorldChunk.java
+++ core/src/com/compilation/game/world/WorldChunk.java
@@ -1,19 +1,68 @@
 package com.compilation.game.world;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.compilation.game.MainGame;
+
+import java.util.ArrayList;
+
+/*
+TODO: add a list of entities here filtered by collidable, or add a listener to when an entity is created with collision
+ or an entity gains collision. Determine if the entity exists within this chunk. If it does, consider it for collision
+ with this chunk and the neighboring 8 chunks.
+ */
 
 public class WorldChunk {
     public static final int CHUNK_SIZE = 64;
-    public static final int TILE_SIZE = 32; // size in pixels
+    public static final int TILE_SIZE = 32; // size in
+
+    private static final int[] backgroundLayers = { 0 };    // list of background layers
+    private static final int[] foregroundLayers = { 1 };    // list of foreground layers
 
     private TiledMap map;
+    private MapRenderer mapRenderer;
+    private MainGame game;
+    private ArrayList<WorldChunk> neighboringChunks;
 
-    public WorldChunk() {
+    private boolean[][] collisionMap; // 2d boolean array for fast access for collision detection
+
+    private long x, y; // x y coordinate of this chunk (from bottom left)
+
+    public WorldChunk(long x, long y, MainGame game) {
+        this.x = x;
+        this.y = y;
+        this.game = game;
+
+        neighboringChunks = new ArrayList<>(8);
+
         map = new TiledMap();
 
-        TiledMapTileLayer collisionLayer = new TiledMapTileLayer(CHUNK_SIZE, CHUNK_SIZE, TILE_SIZE, TILE_SIZE);
-        Cell cell = new Cell();
+        TiledMapTileLayer background = new TiledMapTileLayer(CHUNK_SIZE, CHUNK_SIZE, TILE_SIZE, TILE_SIZE);
+        TiledMapTileLayer foreground = new TiledMapTileLayer(CHUNK_SIZE, CHUNK_SIZE, TILE_SIZE, TILE_SIZE);
+
+        MapLayers layers = map.getLayers();
+        layers.add(background);
+        layers.add(foreground);
+
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 1f/TILE_SIZE, MainGame.batch); // one tile translates to one unit in distance
     }
+
+    public void addNeighboringChunk(WorldChunk neighbor) {
+        neighboringChunks.add(neighbor);
+    }
+
+//    public void renderBackground() {
+//        mapRenderer.render(backgroundLayers);
+//    }
+//
+//    public void renderForeground() {
+//        mapRenderer.render(foregroundLayers);
+//    }
 }
