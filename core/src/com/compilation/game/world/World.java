cf16d3036437eee67030141a3f8a968aeaeac155
Jonah Shader <jonah.shader@gmail.com>
2019-07-30 21:32:58 -0400

    cleaned up code a bit

--- core/src/com/compilation/game/world/World.java
+++ core/src/com/compilation/game/world/World.java
@@ -15,12 +15,11 @@ public class World {
     private WorldGenerator worldGen;
 
     private TextureAtlas worldAtlas;
+
     TiledMapTileLayer.Cell grassCell = new TiledMapTileLayer.Cell();
     TiledMapTileLayer.Cell waterShallowCell = new TiledMapTileLayer.Cell();
     TiledMapTileLayer.Cell waterDarkCell = new TiledMapTileLayer.Cell();
     TiledMapTileLayer.Cell sandCell = new TiledMapTileLayer.Cell();
-
-    private StaticTiledMapTile grassTile, waterShallowTile, waterDarkTile, sandTile;
 
     private MainGame game;
 
@@ -29,10 +28,10 @@ public class World {
 
         worldAtlas = new TextureAtlas("textures/terrain_64x64.pack");
 
-        grassTile = new StaticTiledMapTile(worldAtlas.findRegion("grass"));
-        waterShallowTile = new StaticTiledMapTile(worldAtlas.findRegion("water_shallow"));
-        waterDarkTile = new StaticTiledMapTile(worldAtlas.findRegion("water_deep"));
-        sandTile = new StaticTiledMapTile(worldAtlas.findRegion("sand"));
+        StaticTiledMapTile grassTile = new StaticTiledMapTile(worldAtlas.findRegion("grass"));
+        StaticTiledMapTile waterShallowTile = new StaticTiledMapTile(worldAtlas.findRegion("water_shallow"));
+        StaticTiledMapTile waterDarkTile = new StaticTiledMapTile(worldAtlas.findRegion("water_deep"));
+        StaticTiledMapTile sandTile = new StaticTiledMapTile(worldAtlas.findRegion("sand"));
 
         grassCell.setTile(grassTile);
         waterShallowCell.setTile(waterShallowTile);
@@ -49,42 +48,15 @@ public class World {
                 loadedChunks.add(new WorldChunk(x, y, game, worldGen, this));
             }
         }
-//        loadedChunks.add(new WorldChunk(0, 0, game, worldGen));
     }
 
     public void run() {
-        //TODO: generify the following pseudocode to work on ALL entities that are non static (and the static ones that
-        // have collision, but that might be a little different).
-        //TODO: along with the Collidable component, we should add a Trigger component that has a collision rectangle but
-        // only checks if other entities collided with it. it would be used to trigger events or open doors/etc.
-        /*
-        pseudo code:
-        check if the player has entered a new chunk.
-        if the player has:
-            unload the chunks furthest from the player, and load the new chunks.
-            generate the base world from the worldGen.
-            ask the database if this chunk is unmodified from the WorldGenerator or if its modified.
-            if its modified:
-                download the permutations from the database and apply them over the existing world data from the
-                worldGen.
 
-
-
-        collision detection:
-        keep an array of the collidable entities of the 9 chunks via Family filtering.
-        get the entities and iterate over them to see who is colliding.
-        NOTE: this should probably be implemented into the collision system or into the movement system when those are
-        created.
-         */
     }
 
     public void render(OrthographicCamera cam) {
-//        cam.zoom = 32;
-//        cam.translate(0.9f, 0);
-//        MainGame.batch.setProjectionMatrix(cam.combined);
         cam.update(); // make sure the cam is updated before using it
         for (WorldChunk worldChunk : loadedChunks) {
-
             worldChunk.renderBackground(cam);
         }
     }
